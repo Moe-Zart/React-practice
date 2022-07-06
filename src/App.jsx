627100e4569ab722c4afd69a40a7ffd7b0836ee2
Moe-Zart <mohamedzreika03@outlook.com>
2022-07-06 17:46:40 +1000

    javascript functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import './App.css';
-import Nav from "./components/Nav"
-import MainContent from "./components/MainContent"
-import Footer from "./components/Footer"
+import "./App.css";
+import Nav from "./components/Nav";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+
 function App() {
+  //Javascript
+  const websiteFramework = "React";
+  const websiteTitle = "Practice";
+
+  const date = new Date();
+  const hours = date.getHours();
+  let message = "Good";
+  if (hours < 12) {
+    message += " Morning";
+  } else if (hours >= 12 && hours < 17) {
+    message += " Afternoon";
+  } else {
+    message += " Night";
+  }
+  //
+  //HTML
   return (
-    <div>
+    <div className="App">
+      <div className="javascript-bar">
+        <div>{websiteFramework + "-" + websiteTitle}</div>
+        <div>{message + "!"}</div>
+      </div>
       <Nav />
       <MainContent />
       <Footer />
     </div>
   );
+  //
 }
 
 export default App;
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Nav = () => {
+  const logo = "MZ" //just for the sake of using javascript
   return (
     <nav>
-      <h3 className="logo">MZ</h3>
+      <h3 className="logo">{logo}</h3>
       <ul className="nav-links">
         <li className="nav-link">one</li>
         <li className="nav-link">two</li>
