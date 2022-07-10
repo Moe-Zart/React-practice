6b39306909acfcb175b3e42a32dfc6effd2adc55
Moe-Zart <mohamedzreika03@outlook.com>
2022-07-10 19:31:00 +1000

    more state

--- src/components/ClassComponent.jsx
+++ src/components/ClassComponent.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React from "react";
 
 class ClassComponent extends React.Component {
-    render() {
-        return (
-            <div>
-                <h3>This was rendered by a class</h3>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      name: "Mohamed",
+      age: 19,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h3>This was rendered by:</h3>
+        <div>
+          <h1>{this.state.name}</h1>
+          <h3>{this.state.age} years old</h3>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ClassComponent;
-
-
-
