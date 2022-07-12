b6345a4c7abd68039ff83fa54f7a57cd3a185453
Moe-Zart <mohamedzreika03@outlook.com>
2022-07-12 22:43:12 +1000

    loading state to api fetching

--- src/components/StarWars.jsx
+++ src/components/StarWars.jsx
@@ -5,20 +5,25 @@ class StarWars extends React.Component {
     super();
     this.state = {
       character: {},
+      loading: false, // start loading with false
     };
   }
   async componentDidMount() {
+    this.setState({ loading: true }); //set loading to true until the api is called
     const starData = await fetch("https://swapi.dev/api/people/3");
     const starComponents = await starData.json();
     this.setState({
       character: starComponents,
+      loading: false //set loading back to false since the api is now called
     });
   }
   render() {
     return (
       <div className="star-container">
         <h2 className="starwars-title">my favourite starwars character is:</h2>
-        <h3 className="starwars-character">{this.state.character.name}</h3>
+        <h3 className="starwars-character">
+          {this.state.loading ? "Loading..." : this.state.character.name}
+        </h3>
       </div>
     );
   }
